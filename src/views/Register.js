4428cb17c1f444d7583f9b20ade2feaf683b5622
Matt Ginter <mattginter@gmail.com>
2022-05-02 16:30:53 +0200

    add base view components

---
+++ src/views/Register.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Register() {
+  return (
+    <div>
+      <h1>Register</h1>
+      <input type="text" />
+      <input type="text" />
+      <input type="email" />
+    </div>
+  )
+}
+
+export default Register

3eea342318e695e36ae4a2298233e1f2c81f21ef
yourusername <kunay69@hotmail.it>
2022-05-02 17:05:30 +0200

    Update Homepage.jsx

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 import Button from 'react-bootstrap/Button'
 
 export default function Homepage() {
     return (
         <React.Fragment>
+            <Navbar />
             <Button>
                 Register
             </Button>
             <Button>
                 Log in pls
             </Button>
+            <Footer />
         </React.Fragment>
     )
 }

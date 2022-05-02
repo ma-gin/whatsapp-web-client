d9705af8fdbae0ed2388a395760a4aecfc451a72
yourusername <kunay69@hotmail.it>
2022-05-02 16:46:18 +0200

    skeleton init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import React from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Homepage from "./pages/Homepage"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
 
 export default function App() {
-  return <div>App</div>
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Homepage />} />
+      </Routes>
+      <Footer />
+    </Router>
+  )
 }
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button'
+
+export default function Homepage() {
+    return (
+        <React.Fragment>
+            <Button>
+                Register
+            </Button>
+            <Button>
+                Log in
+            </Button>
+        </React.Fragment>
+    )
+}
+

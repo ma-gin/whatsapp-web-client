b2d38327cf624fe31e423e0b55438c75f1b040d8
yourusername <kunay69@hotmail.it>
2022-05-03 15:18:36 +0200

    sfdgsdg

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Homepage from "./pages/Homepage"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
 import Welcome from "./components/Welcome"
 
 export default function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" exact element={<Homepage />} />
       </Routes>
+      <Footer />
     </Router>
   )
 }

4e5fa1d930114871164892db0d1d9c38dc8877ac
Matt Ginter <mattginter@gmail.com>
2022-05-03 10:09:27 +0200

    add styles

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Homepage from "./pages/Homepage"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
+import Welcome from "./components/Welcome"
 
 export default function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" exact element={<Homepage />} />
+        <Route path="/" exact element={<Welcome />} />
       </Routes>
       <Footer />
     </Router>

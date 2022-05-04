ba19c3c9fea16a90fb49153eeb5d5f818e042683
Matt Ginter <mattginter@gmail.com>
2022-05-04 16:33:07 +0200

    removes hooks from App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import React, { useEffect, useState } from "react"
+import React from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Homepage from "./pages/Homepage.jsx"
 import Welcome from "./components/Welcome.js"
 
 export default function App() {
-  const [access, setAccess] = useState(false)
-  useEffect(() => {
-    checkCookie()
-  }, [])
-
-  const checkCookie = () => {
-    if (document.cookie) setAccess(true)
-  }
   return (
     <Router>
       <Routes>

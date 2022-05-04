08d42d9198dd26a0916cff73f3ef248a5f30bf27
Matt Ginter <mattginter@gmail.com>
2022-05-04 18:18:04 +0200

    map against userId to display correct avatar in chats

--- src/components/SidebarHeader.jsx
+++ src/components/SidebarHeader.jsx
@@ -1,48 +1,50 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react"
 import "../styles/sidebar.css"
 
-import { BiUser } from 'react-icons/bi';
-import { AiOutlinePlus } from 'react-icons/ai';
-import { BsThreeDots } from 'react-icons/bs';
+import { BiUser } from "react-icons/bi"
+import { AiOutlinePlus } from "react-icons/ai"
+import { BsThreeDots } from "react-icons/bs"
 
 export default function SidebarHeader() {
+  const [user, setUser] = useState(undefined)
 
-    const [user, setUser] = useState(undefined)
-
-    const userData = async (event) => {
-        try {
-            console.log('search')
-            const response = await fetch(`${process.env.REACT_APP_USERS_URL}me`, {
-                credentials: "include"
-            })
-            if (response.ok) {
-                const data = await response.json()
-                console.log(data)
-                setUser(data)
-            } else {
-                console.log("error on fetching users")
-            }
-        } catch (error) {
-            console.log(error)
-        }
+  const userData = async (event) => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_USERS_URL}me`, {
+        credentials: "include",
+      })
+      if (response.ok) {
+        const data = await response.json()
+        console.log(data)
+        setUser(data)
+      } else {
+        console.log("error on fetching users")
+      }
+    } catch (error) {
+      console.log(error)
     }
+  }
 
-    useEffect(()=> {
-        userData()
-    }, [])
+  useEffect(() => {
+    userData()
+  }, [])
 
-    return (<>
-    {user && <div className="d-flex align-items-center">
-
-<img src={user.avatar} alt={"User logo"} className={"user-picture me-2"} ></img>
-<p>{user.username}</p>
-<div className="d-flex align-items-center ms-auto">
-    <BiUser />
-    <AiOutlinePlus />
-    <BsThreeDots />
-</div>
-</div>}
-        
-        </>
-    )
+  return (
+    <>
+      {user && (
+        <div className="d-flex align-items-center">
+          <img
+            src={user.avatar}
+            alt={"User logo"}
+            className={"user-picture me-2"}></img>
+          <p>{user.username}</p>
+          <div className="d-flex align-items-center ms-auto">
+            <BiUser />
+            <AiOutlinePlus />
+            <BsThreeDots />
+          </div>
+        </div>
+      )}
+    </>
+  )
 }
--- src/components/SidebarSearch.jsx
+++ src/components/SidebarSearch.jsx
@@ -1,137 +1,164 @@
 import React, { useEffect, useState } from "react"
 import {
-    FormControl,
-    Container,
-    Row,
-    Col,
-    Button,
-    InputGroup,
-    ListGroup,
+  FormControl,
+  Container,
+  Row,
+  Col,
+  Button,
+  InputGroup,
+  ListGroup,
 } from "react-bootstrap"
 
-import { setUserInfoAction } from '../redux/actions'
-import { BsSearch } from 'react-icons/bs';
+import { setUserInfoAction } from "../redux/actions"
+import { BsSearch } from "react-icons/bs"
 import "../styles/sidebar.css"
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux"
 
 export default function SidebarSearch() {
+  const dispatch = useDispatch()
 
-    const dispatch = useDispatch();
+  const [searchedUsers, setSearchedUsers] = useState(undefined)
 
-    const [searchedUsers, setSearchedUsers] = useState(undefined)
+  const [chats, setChats] = useState(undefined)
 
-    const [chats, setChats] = useState(undefined)
+  const user = useSelector((state) => state.userInfo)
 
-    const URL = process.env.REACT_APP_SEARCH_URL
+  const URL = process.env.REACT_APP_SEARCH_URL
 
-    const searchChat = async (event) => {
-        try {
-            console.log('search')
-            const response = await fetch(`${URL}?q=` + event, {
-                credentials: "include"
-            })
-            if (response.ok) {
-                const data = await response.json()
-                setSearchedUsers(data)
-            } else {
-                console.log("error on fetching users")
-                setSearchedUsers(undefined)
-            }
-        } catch (error) {
-            console.log(error)
-        }
+  const searchChat = async (event) => {
+    try {
+      // console.log('search')
+      const response = await fetch(`${URL}?q=` + event, {
+        credentials: "include",
+      })
+      if (response.ok) {
+        const data = await response.json()
+        setSearchedUsers(data)
+      } else {
+        console.log("error on fetching users")
+        setSearchedUsers(undefined)
+      }
+    } catch (error) {
+      console.log(error)
     }
+  }
 
-    const createChat = async (e) => {
-        try {
-            let response = await fetch(`${process.env.REACT_APP_CREATE_CHAT}chats`, {
-                method: "POST",
-                body: JSON.stringify({ recipient: e }),
-                credentials: "include",
-                headers: {
-                    "Content-type": "application/json",
-                },
-            })
-            if (response.ok) {
-                console.log(response)
-            } else {
-                console.log("login failed")
-                if (response.status === 400) {
-                    console.log("bad request")
-                }
-                if (response.status === 404) {
-                    console.log("page not found")
-                }
-            }
-        } catch (error) {
-            console.log(error)
+  const createChat = async (e) => {
+    try {
+      let response = await fetch(`${process.env.REACT_APP_CREATE_CHAT}chats`, {
+        method: "POST",
+        body: JSON.stringify({ recipient: e }),
+        credentials: "include",
+        headers: {
+          "Content-type": "application/json",
+        },
+      })
+      if (response.ok) {
+        console.log(response)
+      } else {
+        console.log("login failed")
+        if (response.status === 400) {
+          console.log("bad request")
         }
-    }
-
-    const existingChats = async (event) => {
-        try {
-            console.log('search')
-            const response = await fetch(`${process.env.REACT_APP_CREATE_CHAT}chats`, {
-                credentials: "include"
-            })
-            if (response.ok) {
-                const data = await response.json()
-                setChats(data)
-            } else {
-                console.log("error on fetching users")
-            }
-        } catch (error) {
-            console.log(error)
+        if (response.status === 404) {
+          console.log("page not found")
         }
+      }
+    } catch (error) {
+      console.log(error)
     }
+  }
 
-    useEffect(() => {
-        existingChats()
-    }, [searchedUsers])
+  const existingChats = async (event) => {
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_CREATE_CHAT}chats`,
+        {
+          credentials: "include",
+        }
+      )
+      if (response.ok) {
+        const data = await response.json()
+        setChats(data)
+      } else {
+        console.log("error on fetching users")
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
-    return (
-        <Container className={"p-0"}>
-            <Row className={" p-0"}>
-                <Col>
-                    <div className="search-input d-flex justify-content-center align-items-center mt-3 ps-3 p-1" >
-                        <BsSearch
-                            className={"me-1"} />
-                        <InputGroup className="w-100 m-0 p-0">
-                            <FormControl
-                                type="search"
-                                placeholder="Search"
-                                className="border-0"
-                                aria-label="Search chat or start a new one"
-                                onChange={e => searchChat(e.target.value)}
-                            />
-                        </InputGroup>
-                    </div>
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    {(searchedUsers === "" || searchedUsers === undefined || searchedUsers === " ") ? <div></div> :
-                        searchedUsers.map((user, idx) => (
-                            <div key={idx}
-                                onClick={async () => { await createChat(user._id); console.log(user); existingChats(); dispatch(setUserInfoAction(user)); }}>{user.username}</div>
-                            // <ListGroup>
-                            //     <ListGroup.Item>
-                            //         <Link to={`/${chat.id}`}>{chat.title}</Link>
-                            //     </ListGroup.Item>
-                            // </ListGroup>
-                        ))}
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    {chats && chats.map((chat, idx) => (
-                        <div onClick={()=> dispatch(setUserInfoAction(chat.members[0]))} key={idx} className="d-flex mt-2 align-items-center">
-                            <img src={chat.members[0].avatar} alt={"User logo"} className={"user-picture  me-2"} ></img>
-                            <p>{chat.members[0].username}</p>
-                        </div>
-                    ))}
-                </Col>
-            </Row>
-        </Container>
-    )
+  useEffect(() => {
+    existingChats()
+  }, [searchedUsers])
+
+  return (
+    <Container className={"p-0"}>
+      <Row className={" p-0"}>
+        <Col>
+          <div className="search-input d-flex justify-content-center align-items-center mt-3 ps-3 p-1">
+            <BsSearch className={"me-1"} />
+            <InputGroup className="w-100 m-0 p-0">
+              <FormControl
+                type="search"
+                placeholder="Search"
+                className="border-0"
+                aria-label="Search chat or start a new one"
+                onChange={(e) => searchChat(e.target.value)}
+              />
+            </InputGroup>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          {searchedUsers === "" ||
+          searchedUsers === undefined ||
+          searchedUsers === " "
+            ? null
+            : searchedUsers.map((user, idx) => (
+                <div
+                  key={idx}
+                  onClick={async () => {
+                    await createChat(user._id)
+                    console.log(user)
+                    existingChats()
+                    dispatch(setUserInfoAction(user))
+                  }}>
+                  {user.username}
+                </div>
+                // <ListGroup>
+                //     <ListGroup.Item>
+                //         <Link to={`/${chat.id}`}>{chat.title}</Link>
+                //     </ListGroup.Item>
+                // </ListGroup>
+              ))}
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          {chats &&
+            chats.map((chat, idx) => {
+              const recipient = chat.members.find(
+                (member) => member._id !== "627149a53ddd922e64b0ec4c" //    ._id
+              )
+              console.log(chats)
+              console.log(user._id)
+              return (
+                <div
+                  onClick={() => dispatch(setUserInfoAction(recipient))}
+                  key={idx}
+                  className="d-flex mt-2 align-items-center">
+                  <img
+                    src={recipient.avatar}
+                    alt={"User logo"}
+                    className={"user-picture  me-2"}></img>
+                  <p>{recipient.username}</p>
+                </div>
+              )
+            })}
+        </Col>
+      </Row>
+    </Container>
+  )
 }
